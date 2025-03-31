c6398d9d827bcd5b4eb5400e60d5f3a214f8b7ac
Doni73 <bwnfo070@gmail.com>
2025-03-31 15:24:20 +0900

    java ex

--- src/ch3/operatorEx16.java
+++ src/ch3/operatorEx16.java
@@ -4,10 +4,18 @@ public class operatorEx16 {
     public static void main(String[] args) {
         float pi = 3.141592f;
         float shortPi = (int)(pi * 1000) / 1000f;
-        System.out.println(shortPi); //3.141
+        System.out.println(shortPi); //3.141 , 셋째자리까지 출력
 
+//소수점 셋째자리에서 반올림
+        double d = 3.141592;
+        double mathPi = Math.round(d * 1000) / 1000.0; //Math.round : 소수점 첫째자리에서 반올림해주는 메서드
+        double mathPi2 = Math.round(d * 1000) / 1000;
+        System.out.println(mathPi); //3.142
+        System.out.println(mathPi2); //3.0
+
+//소수점 셋째자리에서 반올림
         shortPi = (int)(pi * 1000 + 0.5) / 1000f;
-        System.out.println(shortPi); //소수점 셋째자링서 반올림
+        System.out.println(shortPi); //3.142
 
         shortPi = (int)(pi * 100) / 100f;
         System.out.println(shortPi);
